3519682974e040e3e66699a10fab70869a40be59
James Mussah Hamuzah <your-email@example.com>
2026-08-06 10:17:34 +0200

    message

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,10 +12,25 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'role' => \App\Http\Middleware\RoleMiddleware::class,
-        ]);
-    })
+
+    $middleware->alias([
+        'role' => \App\Http\Middleware\RoleMiddleware::class,
+    ]);
+
+    $middleware->redirectGuestsTo(function (Request $request) {
+
+        if ($request->is('admin/*')) {
+            return route('admin.login');
+        }
+
+        if ($request->is('landlord/*')) {
+            return route('landlord.login');
+        }
+
+        return route('landlord.login');
+    });
+
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })
