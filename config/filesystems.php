e3c857bf7d2191be9181e37e5b8dc1f6c3f1b984
James Mussah Hamuzah <your-email@example.com>
2026-08-08 15:53:30 +0200

    m

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,12 +38,12 @@ return [
             'report' => false,
         ],
 
-       'public' => [
-        'driver' => 'local',
-        'root' => storage_path('app/public'),  
-        'url' => env('APP_URL').'/storage',
-        'visibility' => 'public',
-        'throw' => false,
+        'public' => [
+            'driver' => 'local',
+            'root' => public_path('payments'),  // ← CHANGE THIS
+            'url' => env('APP_URL') . '/payments',  // ← CHANGE THIS
+            'visibility' => 'public',
+            'throw' => false,
         ],
 
         's3' => [
