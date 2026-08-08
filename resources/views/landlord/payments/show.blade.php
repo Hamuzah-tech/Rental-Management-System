ec8ca0e7f5a9f4d23e4384b4514a08761c4f147e
James Mussah Hamuzah <your-email@example.com>
2026-08-08 15:13:19 +0200

    i

--- resources/views/landlord/payments/show.blade.php
+++ resources/views/landlord/payments/show.blade.php
@@ -95,11 +95,11 @@
             <h3 class="font-semibold text-sm mb-2 text-[#111827]">Payment Screenshot</h3>
             @if($payment->screenshot)
                 <img 
-                    src="{{ asset('storage/'.$payment->screenshot) }}" 
+                    src="{{ asset('payments/'.$payment->screenshot) }}" 
                     class="rounded-lg border w-full h-32 object-cover cursor-pointer"
-                    onclick="openModal('{{ asset('storage/'.$payment->screenshot) }}')">
+                    onclick="openModal('{{ asset('payments/'.$payment->screenshot) }}')">
                 <button 
-                    onclick="openModal('{{ asset('storage/'.$payment->screenshot) }}')"
+                    onclick="openModal('{{ asset('payments/'.$payment->screenshot) }}')"
                     class="mt-2 text-[#ca0251] hover:text-[#a80244] text-sm text-center transition">
                     Open Full Image
                 </button>
